3b11dd7504ff0ff5e7c3def63a5b8e4c08178dc4
David Grinberg <green.grinya@gmail.com>
2026-01-23 17:13:54 +0700

    Fix TableID assignment in PSISection

--- muxer.go
+++ muxer.go
@@ -343,7 +343,7 @@ func (m *Muxer) generatePAT() error {
 		Header: &PSISectionHeader{
 			SectionLength:          calcPATSectionLength(d),
 			SectionSyntaxIndicator: true,
-			TableID:                PSITableID(d.TransportStreamID),
+			TableID:                PSITableID(PSITableIDPAT),
 		},
 		Syntax: syntax,
 	}
